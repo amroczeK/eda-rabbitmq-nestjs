cb15e1f709583e7b6231c75a9079a12cf96eb94c
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2023-07-28 22:10:13 +0800

    env files and config module, joi schema

--- apps/inventory-service/src/inventory-service.module.ts
+++ apps/inventory-service/src/inventory-service.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { InventoryServiceController } from './inventory-service.controller';
 import { InventoryServiceService } from './inventory-service.service';
 import { RabbitMqModule } from '@app/common/rabbit-mq/rabbit-mq.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [RabbitMqModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    ,
+    RabbitMqModule,
+  ],
   controllers: [InventoryServiceController],
   providers: [InventoryServiceService],
 })
--- apps/order-service/src/order-service.module.ts
+++ apps/order-service/src/order-service.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { OrderServiceController } from './order-service.controller';
 import { OrderServiceService } from './order-service.service';
 import { RabbitMqModule } from '@app/common/rabbit-mq/rabbit-mq.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [RabbitMqModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    ,
+    RabbitMqModule,
+  ],
   controllers: [OrderServiceController],
   providers: [OrderServiceService],
 })
