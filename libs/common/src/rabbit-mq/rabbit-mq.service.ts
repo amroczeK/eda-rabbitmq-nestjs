b33b0c46504d413d7ae6acd30ebafdd8d8e06a85
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2023-08-11 20:08:56 +0800

    rabbitmq service, docker compose healthchecks

--- libs/common/src/rabbit-mq/rabbit-mq.service.ts
+++ libs/common/src/rabbit-mq/rabbit-mq.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RmqContext, RmqOptions, Transport } from '@nestjs/microservices';
 import { connect, Connection, Channel } from 'amqplib';
@@ -7,30 +7,97 @@ import { connect, Connection, Channel } from 'amqplib';
 export class RabbitMqService {
   private connection: Connection;
   private channel: Channel;
+  private readonly exchangeName = 'shop.topic';
+  private readonly exchangeType = 'topic';
+  private readonly logger = new Logger(RabbitMqService.name);
+  private readonly maxRetries = 5;
+  private readonly retryDelay = 5000; // 5 seconds
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {
+    this.init();
+  }
 
-  async connect() {
+  private async init() {
+    let retries = 0;
+    while (retries < this.maxRetries) {
+      try {
+        await this.connect();
+        this.connection.on('close', () => {
+          this.logger.error(
+            'RabbitMQ connection closed! Attempting to reconnect...',
+          );
+          setTimeout(() => this.init(), this.retryDelay);
+        });
+        this.connection.on('error', (err) => {
+          this.logger.error(`RabbitMQ error: ${err.message}`);
+        });
+        break;
+      } catch (err) {
+        this.logger.error(
+          `Failed to connect to RabbitMQ. Retrying in ${
+            this.retryDelay / 1000
+          } seconds...`,
+        );
+        await new Promise((res) => setTimeout(res, this.retryDelay));
+        retries += 1;
+      }
+    }
+    if (retries === this.maxRetries) {
+      this.logger.error('Max retries reached. Could not connect to RabbitMQ.');
+    }
+  }
+
+  private async connect() {
     this.connection = await connect(
       this.configService.get<string>('RABBIT_MQ_URI'),
     );
     this.channel = await this.connection.createChannel();
+
+    await this.channel.assertExchange(this.exchangeName, this.exchangeType, {
+      durable: true,
+    });
+    const inventoryQueue = 'inventory';
+    const orderQueue = 'order';
+    await this.channel.assertQueue(inventoryQueue, { durable: true });
+    await this.channel.assertQueue(orderQueue, { durable: true });
+    this.channel.bindQueue(
+      inventoryQueue,
+      this.exchangeName,
+      'shop.inventory.*',
+    );
+    this.channel.bindQueue(
+      inventoryQueue,
+      this.exchangeName,
+      'shop.order.created',
+    );
+    this.channel.bindQueue(orderQueue, this.exchangeName, 'shop.order.placed');
   }
 
-  async publishMessage(queue: string, message: string) {
-    await this.channel.assertQueue(queue, { durable: false });
-    this.channel.sendToQueue(queue, Buffer.from(message));
+  async publishMessage(routingKey: string, message: string) {
+    try {
+      this.channel.publish(this.exchangeName, routingKey, Buffer.from(message));
+    } catch (err) {
+      this.logger.error(`Failed to publish message: ${err.message}`);
+    }
   }
 
   async close() {
-    await this.channel.close();
-    await this.connection.close();
+    try {
+      await this.channel.close();
+      await this.connection.close();
+    } catch (err) {
+      this.logger.error(`Error while closing the connection: ${err.message}`);
+    }
   }
 
   ack(context: RmqContext) {
-    const channel = context.getChannelRef();
-    const originalMessage = context.getMessage();
-    channel.ack(originalMessage);
+    try {
+      const channel = context.getChannelRef();
+      const originalMessage = context.getMessage();
+      channel.ack(originalMessage);
+    } catch (err) {
+      this.logger.error(`Failed to acknowledge message: ${err.message}`);
+    }
   }
 
   getOptions(queue: string, noAck = false): RmqOptions {
